5960c8bb088054d003e3c49b92f7c30db172f2c1
Rohan Shah <rohanshah01@gmail.com>
2020-11-11 02:22:55 -0500

    create simulate function
    Basic Agent 3 with an iteration counter. Please refer to comments to help determine a proper optimization calculation

--- improvedAgent.py
+++ improvedAgent.py
@@ -1,7 +1,47 @@
 import random as rnd
 import numpy as np
+import copy 
 from common import Agent, Target, numActions, manhattanDistance
 
+def simulateBA(agent,scale,r,c,it): 
+    # current performance: iterating even 1 step into the future at every time step is causing failure.
+    # this has to do with the fact that even when the probabilities are very high we still assume that there is a failure occuring
+    # adding an addl conditional to check the prob value and skipping recursion if above thresh
+    minScore = agent.dim**4
+    new_r = r
+    new_c = c
+    i = 0
+    while i < agent.dim:
+        j = 0
+        while j < agent.dim:
+            if i == r and j == c:
+                j += 1
+                continue
+            agent.map[i][j].probability = agent.map[i][j].probability / scale
+            dist = manhattanDistance((r,c), (i,j))
+            agent.map[i][j].score = float(dist) / (agent.map[i][j].probability * (1 - agent.map[i][j].falseNegativeProbability))
+            if ((minScore > agent.map[i][j].score)):
+                minScore = agent.map[i][j].score
+                new_r = i
+                new_c = j
+            j = j + 1
+        i = i + 1
+    
+    if(it < 1 and agent.map[r][c].probability < 0.2): 
+        '''
+            TODO change conditional to following form: A - B*x (obviously if this makes sense)
+            A = max iterations you allow on the first run through (ie we don't have enough info so we let the agent simulate several failures)
+            B = how gradual we want the descent to be. 
+            Other considerations: should B be a dynamic value? depending on the current performance, can we tweak this value? 
+        '''
+        nit = it
+        nit+=1
+        scale = 1.0 - agent.map[new_r][new_c].probability + agent.map[new_r][new_c].probability * agent.map[new_r][new_c].falseNegativeProbability
+        agent2 = copy.deepcopy(agent) #is there any way to not use a deep copy here? 
+        agent2.map[new_r][new_c].probability = agent2.map[new_r][new_c].falseNegativeProbability * agent2.map[new_r][new_c].probability
+        return simulateBA(agent2,scale,new_r,new_c,nit)
+    
+    return (new_r, new_c)
 
 
 def improvedAgent(agent, target):
@@ -17,75 +57,15 @@ def improvedAgent(agent, target):
                 r, c = cell.row, cell.col
 
     while (agent.hasFoundTarget == False):
-        minScore = agent.dim**4
         prevr = r
         prevc = c
         searchResult = agent.searchCell(r,c, target)
         if searchResult == False:
-            scale = 1 - agent.map[r][c].probability + agent.map[r][c].probability * agent.map[r][c].falseNegativeProbability
+            scale = 1.0 - agent.map[r][c].probability + agent.map[r][c].probability * agent.map[r][c].falseNegativeProbability
             agent.map[r][c].probability = agent.map[r][c].falseNegativeProbability * agent.map[r][c].probability
-            i = 0
-            while i < agent.dim:
-                j = 0
-                while j < agent.dim:
-                    if i == prevr and j == prevc:
-                        j += 1
-                        continue
-                    agent.map[i][j].probability = agent.map[i][j].probability / scale
-                    dist = manhattanDistance((prevr,prevc), (i,j))
-                    agent.map[i][j].score = float(dist) / (agent.map[i][j].probability * (1 - agent.map[i][j].falseNegativeProbability))
-                    if minScore > agent.map[i][j].score:
-                        minScore = agent.map[i][j].score
-                        r = i
-                        c = j
-                    j = j + 1
-                i = i + 1
-            r,c = improvementFunction(agent, minScore, prevr, prevc, r, c)
+            r,c = simulateBA(agent,scale,r,c,0)
             numActions((prevr, prevc), (r,c), agent)
-
     return agent.numMoves
-'''
-What we need: 
--minScore
--Standard deviation of set
-go one st dev above the minscore
-get all the coordinates of scores between, inclusive, of minscore and minscore+1stdev
-rank all scores by distance (recalculate dist using coordinates and prevr and prevc)
-select the highest score with the lowest manhattan distance, visit there
-
-improvementFunction(agent.map, minScore, msr, msc, prevr, prevc) returns (nextr,nextc) 
-
-'''
-
-def improvementFunction(agent, minScore, prevr, prevc, msr, msc):  
-    arrayMap = []
-    for row in agent.map:
-        for i in row:
-            arrayMap.append(i.score)
-    std = np.std(arrayMap)
-    currentBestRow = -1
-    currentBestColumn = -1
-    currentBestScore = -1
-    currentMD = -1 
-    for row in agent.map: 
-        for i in row: 
-            if(i.score <= minScore+(0.5*std)):
-                cmd = manhattanDistance((prevr,prevc),(i.row,i.col))
-                if((i.row == prevr and i.col == prevc) or (i.row == msr and i.col == msc)):
-                    continue
-                if(currentBestScore == -1 and currentMD == -1):
-                    currentMD = cmd
-                    currentBestScore = i.score
-                    currentBestRow = i.row
-                    currentBestColumn = i.col
-                elif(i.score < currentBestScore and cmd < currentMD): 
-                    currentMD = cmd
-                    currentBestScore = i.score 
-                    currentBestRow = i.row
-                    currentBestColumn = i.col
-                else:
-                    continue
-    return (currentBestRow, currentBestColumn)
 
 def displayScores(agent):
     for r in agent.map:
@@ -102,7 +82,7 @@ def displayProbabilities(agent):
 
 
 total = 0
-numTrials = 100
+numTrials = 50
 dim = 10
 for i in range(numTrials):
     agent = Agent(dim)
